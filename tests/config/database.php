e9f71e44d595775380c536cb05ebfbaeb821476b
Gary Pearman <gpearman@gmail.com>
2015-12-27 12:42:17 +0000

    Attempt at Travis CI database fix

--- tests/config/database.php
+++ tests/config/database.php
@@ -1,32 +1,26 @@
 <?php
 
-// Test against in-memory SQLite DB
-return array(
-  'driver'    => 'sqlite',
-  'database'  => ':memory:',
-  'prefix'    => ''
-);
+$db = getenv('DB');
 
-// // Test against local Postgres
-// return array(
-//   'driver'   => 'pgsql',
-//   'host'     => 'localhost',
-//   'database' => 'baum_test',
-//   'username' => 'postgres',
-//   'password' => 'postgres',
-//   'charset'  => 'utf8',
-//   'prefix'   => '',
-//   'schema'   => 'public',
-// );
+if($db == 'travis') {
+    $config = array(
+        'driver'    => 'mysql',
+        'host'      => 'localhost',
+        'database'  => 'baum_testing',
+        'username'  => 'root',
+        'password'  => '',
+        'charset'   => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix'    => ''
+    );
+}
 
-// // Test against local MySQL
-// return array(
-//   'driver'   => 'mysql',
-//   'host'     => 'localhost',
-//   'database' => 'baum_test',
-//   'username' => 'mysql',
-//   'password' => 'mysql',
-//   'charset'   => 'utf8',
-//   'collation' => 'utf8_unicode_ci',
-//   'prefix'    => '',
-// );
+if(!$db) {
+    $config = array(
+        'driver'    => 'sqlite',
+        'database'  => ':memory:',
+        'prefix'    => ''
+    );
+}
+
+return $config;
