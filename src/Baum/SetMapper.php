f3e2311fcee3bf6217c0f98b678360e1cf109207
Gary Pearman <gpearman@gmail.com>
2016-01-19 21:38:19 +0000

    Formatting

--- src/Baum/SetMapper.php
+++ src/Baum/SetMapper.php
@@ -8,145 +8,141 @@ use Illuminate\Support\Contracts\ArrayableInterface;
 class SetMapper
 {
     /**
-   * Node instance for reference.
-   *
-   * @var \Baum\Node
-   */
-  protected $node = null;
+    * Node instance for reference.
+    *
+    * @var \Baum\Node
+    */
+    protected $node = null;
 
-  /**
-   * Children key name.
-   *
-   * @var string
-   */
-  protected $childrenKeyName = 'children';
+    /**
+     * Children key name.
+     *
+     * @var string
+     */
+    protected $childrenKeyName = 'children';
 
-  /**
-   * Create a new \Baum\SetBuilder class instance.
-   *
-   * @param   \Baum\Node      $node
-   * @return  void
-   */
-  public function __construct($node, $childrenKeyName = 'children')
-  {
-      $this->node = $node;
+    /**
+     * Create a new \Baum\SetBuilder class instance.
+     *
+     * @param   \Baum\Node      $node
+     * @return  void
+     */
+    public function __construct($node, $childrenKeyName = 'children')
+    {
+        $this->node = $node;
 
-      $this->childrenKeyName = $childrenKeyName;
-  }
+        $this->childrenKeyName = $childrenKeyName;
+    }
 
-  /**
-   * Maps a tree structure into the database. Unguards & wraps in transaction.
-   *
-   * @param   array|\Illuminate\Support\Contracts\ArrayableInterface
-   * @return  bool
-   */
-  public function map($nodeList)
-  {
-      $self = $this;
+    /**
+     * Maps a tree structure into the database. Unguards & wraps in transaction.
+     *
+     * @param   array|\Illuminate\Support\Contracts\ArrayableInterface
+     * @return  bool
+     */
+    public function map($nodeList)
+    {
+        $self = $this;
 
-      return $this->wrapInTransaction(function () use ($self, $nodeList) {
-      forward_static_call([get_class($self->node), 'unguard']);
+        return $this->wrapInTransaction(function () use ($self, $nodeList) {
+            forward_static_call([get_class($self->node), 'unguard']);
+            $result = $self->mapTree($nodeList);
+            forward_static_call([get_class($self->node), 'reguard']);
 
-      $result = $self->mapTree($nodeList);
+            return $result;
+        });
+    }
 
-      forward_static_call([get_class($self->node), 'reguard']);
+    /**
+     * Maps a tree structure into the database without unguarding nor wrapping
+     * inside a transaction.
+     *
+     * @param   array|\Illuminate\Support\Contracts\ArrayableInterface
+     * @return  bool
+     */
+    public function mapTree($nodeList)
+    {
+        $tree = $nodeList instanceof ArrayableInterface ? $nodeList->toArray() : $nodeList;
 
-      return $result;
-    });
-  }
+        $affectedKeys = [];
 
-  /**
-   * Maps a tree structure into the database without unguarding nor wrapping
-   * inside a transaction.
-   *
-   * @param   array|\Illuminate\Support\Contracts\ArrayableInterface
-   * @return  bool
-   */
-  public function mapTree($nodeList)
-  {
-      $tree = $nodeList instanceof ArrayableInterface ? $nodeList->toArray() : $nodeList;
+        $result = $this->mapTreeRecursive($tree, $this->node->getKey(), $affectedKeys);
 
-      $affectedKeys = [];
-
-      $result = $this->mapTreeRecursive($tree, $this->node->getKey(), $affectedKeys);
-
-      if ($result && count($affectedKeys) > 0) {
-          $this->deleteUnaffected($affectedKeys);
-      }
-
-      return $result;
-  }
-
-  /**
-   * Returns the children key name to use on the mapping array.
-   *
-   * @return string
-   */
-  public function getChildrenKeyName()
-  {
-      return $this->childrenKeyName;
-  }
-
-  /**
-   * Maps a tree structure into the database.
-   *
-   * @param   array   $tree
-   * @param   mixed   $parent
-   * @return  bool
-   */
-  protected function mapTreeRecursive(array $tree, $parentKey = null, &$affectedKeys = [])
-  {
-      // For every attribute entry: We'll need to instantiate a new node either
-    // from the database (if the primary key was supplied) or a new instance. Then,
-    // append all the remaining data attributes (including the `parent_id` if
-    // present) and save it. Finally, tail-recurse performing the same
-    // operations for any child node present. Setting the `parent_id` property at
-    // each level will take care of the nesting work for us.
-    foreach ($tree as $attributes) {
-        $node = $this->firstOrNew($this->getSearchAttributes($attributes));
-
-        $data = $this->getDataAttributes($attributes);
-        if (! is_null($parentKey)) {
-            $data[$node->getParentColumnName()] = $parentKey;
+        if ($result && count($affectedKeys) > 0) {
+            $this->deleteUnaffected($affectedKeys);
         }
 
-        $node->fill($data);
+        return $result;
+    }
 
-        $result = $node->save();
+    /**
+     * Returns the children key name to use on the mapping array.
+     *
+     * @return string
+     */
+    public function getChildrenKeyName()
+    {
+        return $this->childrenKeyName;
+    }
 
-        if (! $result) {
-            return false;
-        }
+    /**
+     * Maps a tree structure into the database.
+     *
+     * @param   array   $tree
+     * @param   mixed   $parent
+     * @return  bool
+     */
+    protected function mapTreeRecursive(array $tree, $parentKey = null, &$affectedKeys = [])
+    {
+        // For every attribute entry: We'll need to instantiate a new node either
+        // from the database (if the primary key was supplied) or a new instance. Then,
+        // append all the remaining data attributes (including the `parent_id` if
+        // present) and save it. Finally, tail-recurse performing the same
+        // operations for any child node present. Setting the `parent_id` property at
+        // each level will take care of the nesting work for us.
+        foreach ($tree as $attributes) {
+            $node = $this->firstOrNew($this->getSearchAttributes($attributes));
 
-        $affectedKeys[] = $node->getKey();
+            $data = $this->getDataAttributes($attributes);
+            if (! is_null($parentKey)) {
+                $data[$node->getParentColumnName()] = $parentKey;
+            }
 
-        if (array_key_exists($this->getChildrenKeyName(), $attributes)) {
-            $children = $attributes[$this->getChildrenKeyName()];
+            $node->fill($data);
 
-            if (count($children) > 0) {
-                $result = $this->mapTreeRecursive($children, $node->getKey(), $affectedKeys);
+            $result = $node->save();
 
-                if (! $result) {
-                    return false;
+            if (! $result) {
+                return false;
+            }
+
+            $affectedKeys[] = $node->getKey();
+
+            if (array_key_exists($this->getChildrenKeyName(), $attributes)) {
+                $children = $attributes[$this->getChildrenKeyName()];
+
+                if (count($children) > 0) {
+                    $result = $this->mapTreeRecursive($children, $node->getKey(), $affectedKeys);
+
+                    if (!$result) {
+                        return false;
+                    }
                 }
             }
         }
-    }
 
-      return true;
-  }
+        return true;
+    }
 
     protected function getSearchAttributes($attributes)
     {
         $searchable = [$this->node->getKeyName()];
-
         return array_only($attributes, $searchable);
     }
 
     protected function getDataAttributes($attributes)
     {
         $exceptions = [$this->node->getKeyName(), $this->getChildrenKeyName()];
-
         return array_except($attributes, $exceptions);
     }
 
