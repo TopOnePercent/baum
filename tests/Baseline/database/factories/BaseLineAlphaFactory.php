a623bb369d726272f25750c640978c4d7bc40dcf
poing <brian.lavallee@invite-comm.jp>
2020-04-20 11:34:39 +0900

    Expanding reworked tests.

--- tests/Baseline/database/factories/BaseLineAlphaFactory.php
+++ tests/Baseline/database/factories/BaseLineAlphaFactory.php
@@ -3,7 +3,6 @@
 use Faker\Generator as Faker;
 use Baum\Tests\Baseline\Models\BaseLineAlpha;
 
-
 $factory->define(BaseLineAlpha::class, function (Faker $faker) {
     return [
         'value' => $faker->sentence(6)
