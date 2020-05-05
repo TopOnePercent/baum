b5ccbea343a2a97d3635a1224910625c29fb9583
Brian LaVallee <brian.lavallee@invite-comm.jp>
2020-05-05 10:12:50 +0000

    Apply fixes from StyleCI

--- src/Baum/Contracts/Questions.php
+++ src/Baum/Contracts/Questions.php
@@ -4,18 +4,25 @@ namespace Baum\Contracts;
 
 interface Questions
 {
+    public function isRoot();
 
-	public function isRoot();
-	public function isLeaf();
-	public function isChild();
-	public function isChildOf($other);
-	public function isDescendantOf($other);
-	public function isSelfOrDescendantOf($other);
-	public function isAncestorOf($other);
-	public function isSelfOrAncestorOf($other);
-	public function equals($node);
-	public function insideSubtree($node);
-	public function inSameScope($node);
+    public function isLeaf();
 
+    public function isChild();
 
+    public function isChildOf($other);
+
+    public function isDescendantOf($other);
+
+    public function isSelfOrDescendantOf($other);
+
+    public function isAncestorOf($other);
+
+    public function isSelfOrAncestorOf($other);
+
+    public function equals($node);
+
+    public function insideSubtree($node);
+
+    public function inSameScope($node);
 }
